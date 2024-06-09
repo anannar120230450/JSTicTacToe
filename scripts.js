d0091267c785f5e1bccb4114c7770b4f3d55bc43
anannar120230450 <anannar120230450>
2024-06-09 18:04:52 +0100

    create a system so that you only click one box once

--- scripts.js
+++ scripts.js
@@ -3,6 +3,13 @@ function GameInit() {
     let currentTurn = players[0];
     const getPlayers = () => players;
     const getCurrentTurn = () => currentTurn;
+    const updateCurrentTurn = () => {
+        if (currentTurn === players[0]) {
+            currentTurn = players[1]
+        } else if (currentTurn === players[1]) {
+            currentTurn = players[0]
+        };
+    }
     const gameGridArray = [
         "", "", "",
         "", "", "",
@@ -23,7 +30,8 @@ function GameInit() {
         [gameGridArray[6], gameGridArray[7], gameGridArray[8]],
     ];
     return {
-        getPlayers, getCurrentTurn
+        getPlayers, getCurrentTurn,
+        updateCurrentTurn
     };
 };
 
@@ -31,4 +39,15 @@ const Game = GameInit();
 
 window.addEventListener("DOMContentLoaded", () => {
     const ticTacToeGameSquaresGridSquareItems = [...document.querySelectorAll(".tic-tac-toe-game-squares-grid-square-item")];
+
+    ticTacToeGameSquaresGridSquareItems.forEach(squareItem => {
+        let squareItemChosen = false;
+        squareItem.addEventListener("click", () => {
+            if (!squareItemChosen) {
+                squareItem.innerHTML = Game.getCurrentTurn();
+                Game.updateCurrentTurn();
+                squareItemChosen = true;
+            };
+        });
+    });
 });
