5592bba7eac8ca5e8cda7ff562311175fa163a7b
anannar120230450 <anannar120230450>
2024-06-10 16:43:10 +0100

    add a check to see if a cell is already selected and change 0 to O

--- scripts.js
+++ scripts.js
@@ -55,33 +55,64 @@ function GameCell(id, value = "", selected = false) {
 }
 
 const DOMCellItems = [...document.querySelectorAll(".tic-tac-toe-game-squares-grid-square-item")];
-const Game = GameInit("X", "0");
+const Game = GameInit("X", "O");
 
 DOMCellItems.forEach((cellItem, index) => {
+    let cellSelected = false;
     cellItem.addEventListener("click", () => {
-        cellItem.innerHTML = Game.getCurrentPlayer();
-        Game.updateCellsArray(index, Game.getCurrentPlayer(), true);
-        // console.log(Game.getCellsArray());
-        // check winner
-        const currentCellArray = Game.getCellsArray();
-        const winningCellSequences = [
-            // left to right
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            // top to bottom
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            // diagonals
-            [0, 4, 9], // top left to bottom right
-            [2, 4, 6], // top right to bottom left
-        ]
-        
-        let didAnyWin = false;
-        for (cellSequence of winningCellSequences) {
-            const [a, b, c] = cellSequence;
-            if (currentCellArray[a].value && currentCellArray[a].value === currentCellArray[b].value && currentCellArray[a].value === currentCellArray[c].value) {
+        if (!cellSelected) {
+            cellSelected = true;
+            cellItem.innerHTML = Game.getCurrentPlayer();
+            Game.updateCellsArray(index, Game.getCurrentPlayer(), true);
+            // console.log(Game.getCellsArray());
+            // check winner
+            const currentCellArray = Game.getCellsArray();
+            const winningCellSequences = [
+                // left to right
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                // top to bottom
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                // diagonals
+                [0, 4, 9], // top left to bottom right
+                [2, 4, 6], // top right to bottom left
+            ]
+
+            let didAnyWin = false;
+            for (cellSequence of winningCellSequences) {
+                const [a, b, c] = cellSequence;
+                if (currentCellArray[a].value && currentCellArray[a].value === currentCellArray[b].value && currentCellArray[a].value === currentCellArray[c].value) {
+                    DOMCellItems.forEach(item => {
+                        item.innerHTML = "";
+                    });
+                    if (Game.getCurrentPlayer() === "X") {
+                        Game.updatePlayerScore(1);
+                        document.querySelector(".game-info-toolbar-player-scores-player-1-score").innerHTML = Game.getPlayerScore(1);
+
+                        // check if won
+                        if (Game.getPlayerScore(1) === 3) {
+                            document.querySelector(".winning-message-backdrop").style.display = "flex";
+                        }
+                    } else if (Game.getCurrentPlayer() === "0") {
+                        Game.updatePlayerScore(2);
+                        document.querySelector(".game-info-toolbar-player-scores-player-2-score").innerHTML = Game.getPlayerScore(2);
+                    };
+                    Game.reset();
+                    didAnyWin = true;
+                    break;
+
+                    /* document.getElementById("winning-message-text").innerHTML = `${Game.getCurrentPlayer()} Wins`;
+                    document.querySelector(".winning-message-backdrop").style.display = "flex"; */
+                }
+            }
+
+            if (!didAnyWin) {
+                Game.updateCurrentPlayer()
+            }
+            /*if (currentCellArray[0].value && currentCellArray[0].value === currentCellArray[1].value && currentCellArray[0].value === currentCellArray[2].value) {
                 DOMCellItems.forEach(item => {
                     item.innerHTML = "";
                 });
@@ -93,34 +124,12 @@ DOMCellItems.forEach((cellItem, index) => {
                     document.querySelector(".game-info-toolbar-player-scores-player-2-score").innerHTML = Game.getPlayerScore(2);
                 };
                 Game.reset();
-                didAnyWin = true;
-                break;
     
-                /* document.getElementById("winning-message-text").innerHTML = `${Game.getCurrentPlayer()} Wins`;
-                document.querySelector(".winning-message-backdrop").style.display = "flex"; */
-            }
-        }
-
-        if (!didAnyWin) {
-            Game.updateCurrentPlayer()
-        }
-        /*if (currentCellArray[0].value && currentCellArray[0].value === currentCellArray[1].value && currentCellArray[0].value === currentCellArray[2].value) {
-            DOMCellItems.forEach(item => {
-                item.innerHTML = "";
-            });
-            if (Game.getCurrentPlayer() === "X") {
-                Game.updatePlayerScore(1);
-                document.querySelector(".game-info-toolbar-player-scores-player-1-score").innerHTML = Game.getPlayerScore(1);
-            } else if (Game.getCurrentPlayer() === "0") {
-                Game.updatePlayerScore(2);
-                document.querySelector(".game-info-toolbar-player-scores-player-2-score").innerHTML = Game.getPlayerScore(2);
-            };
-            Game.reset();
-
-            /// document.getElementById("winning-message-text").innerHTML = `${Game.getCurrentPlayer()} Wins`;
-            document.querySelector(".winning-message-backdrop").style.display = "flex"; ///
-        } else {
-            Game.updateCurrentPlayer();
-        };*/
+                /// document.getElementById("winning-message-text").innerHTML = `${Game.getCurrentPlayer()} Wins`;
+                document.querySelector(".winning-message-backdrop").style.display = "flex"; ///
+            } else {
+                Game.updateCurrentPlayer();
+            };*/
+        };
     });
 });
